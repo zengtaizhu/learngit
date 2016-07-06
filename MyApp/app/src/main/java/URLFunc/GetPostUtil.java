d61c00126a0ffc505ecc11ba526da8f5354e2040
zengtaizhu <zengtaizhu@qq.com>
2016-07-06 23:47:50 +0800

    增加了GetPostUtil.java中的puttt方法

--- MyApp/app/src/main/java/URLFunc/GetPostUtil.java
+++ MyApp/app/src/main/java/URLFunc/GetPostUtil.java
@@ -92,7 +92,9 @@ public class GetPostUtil {
             // 设置通用的请求属性
             if(null != JSESSIONID)
             {
+                //当存在JSESSIONID时，即已经登录后，请求的属性要加上JSESSIONID以及请求的内容改为json
                 conn.setRequestProperty("Cookie", "JSESSIONID="+JSESSIONID);
+                conn.setRequestProperty("Content-Type","application/json");
             }
             // 发送POST请求必须设置如下两行
             conn.setDoOutput(true);
@@ -163,10 +165,6 @@ public class GetPostUtil {
             {
                 conn.setRequestProperty("Cookie", "JSESSIONID="+JSESSIONID);
             }
-            conn.setRequestProperty("accept", "*/*");
-            conn.setRequestProperty("connection", "Keep-Alive");
-            conn.setRequestProperty("user-agent",
-                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)");
             conn.setRequestProperty("Content-Type","application/json");
             // 建立实际的连接
             conn.connect();
@@ -189,6 +187,75 @@ public class GetPostUtil {
         {
             try
             {
+                if (in != null)
+                {
+                    in.close();
+                }
+            }
+            catch (IOException ex)
+            {
+                ex.printStackTrace();
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 向指定URL发送Put方法的请求
+     * @param url 发送请求的URL
+     * @param params
+     * @param JSESSIONID
+     */
+    public static String sendPut(String url,String params, String JSESSIONID)
+    {
+        PrintWriter out = null;
+        BufferedReader in = null;
+        String result = "";
+        try
+        {
+            URL realUrl = new URL(url);
+            // 打开和URL之间的连接
+            HttpURLConnection conn = (HttpURLConnection)realUrl.openConnection();
+            conn.setRequestMethod("PUT");
+            conn.setUseCaches(false);
+            conn.setInstanceFollowRedirects(true);
+            // 设置通用的请求属性
+            if(null != JSESSIONID)
+            {
+                conn.setRequestProperty("Cookie", "JSESSIONID="+JSESSIONID);
+                conn.setRequestProperty("Content-Type","application/json");
+            }
+            // 发送PUT请求必须设置如下
+            conn.setDoOutput(true);
+            // 获取URLConnection对象对应的输出流
+            out = new PrintWriter(conn.getOutputStream());
+            // 发送请求参数
+            out.print(params);
+            // flush输出流的缓冲
+            out.flush();
+            // 定义BufferedReader输入流来读取URL的响应
+            in = new BufferedReader(
+                    new InputStreamReader(conn.getInputStream(),"utf-8"));
+            String line;
+            while ((line = in.readLine()) != null)
+            {
+                result += line;
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println("发送Put请求出现异常！" + e);
+            e.printStackTrace();
+        }
+        // 使用finally块来关闭输出流、输入流
+        finally
+        {
+            try
+            {
+                if (out != null)
+                {
+                    out.close();
+                }
                 if (in != null)
                 {
                     in.close();
